e38f6a692b6e229d5162ff10d243239385a12a65
kevinrrdev0 <kevinrrdev@gmail.com>
2022-08-14 22:30:47 -0500

    changes in info order

--- core-ui/src/main/java/gsg/corp/core_ui/Color.kt
+++ core-ui/src/main/java/gsg/corp/core_ui/Color.kt
@@ -11,8 +11,23 @@ val FatColor = Color(0xFFF44336)
 val MediumGray = Color(0xFF404040)
 val DarkGray = Color(0xFF202020)
 val TextWhite = Color(0xFFEEEEEE)
+val ColorWhite = Color(0xFFEEEEEE)
 
 val ColorSuccess = Color(0xFF22BB33)
 val ColorReject = Color(0xFFB00020)
 val ColorRescheduled =  Color(0xFFFFAA00)
 val ColorRegister = Color(0xFF808080)
+
+val ColorCall = Color(0xFF2196F3)
+
+val Green = Color(0xFF1CB42D)
+
+
+//<color name="primaryColor">#ec0000</color>
+//<color name="primaryLightColor">#ff5633</color>
+//<color name="primaryDarkColor">#b00000</color>
+//<color name="secondaryColor">#ffffff</color>
+//<color name="secondaryLightColor">#ffffff</color>
+//<color name="secondaryDarkColor">#cccccc</color>
+//<color name="primaryTextColor">#ffffff</color>
+//<color name="secondaryTextColor">#000000</color>
