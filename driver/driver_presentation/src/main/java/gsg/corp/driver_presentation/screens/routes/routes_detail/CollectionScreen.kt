e5c43b6bb8284c76ae7ff91bdd3bb38383261da2
danieliopx <danielfloresrojas1@gmail.com>
2023-02-16 15:41:44 -0500

    implement galleria button

--- driver/driver_presentation/src/main/java/gsg/corp/driver_presentation/screens/routes/routes_detail/CollectionScreen.kt
+++ driver/driver_presentation/src/main/java/gsg/corp/driver_presentation/screens/routes/routes_detail/CollectionScreen.kt
@@ -1,14 +1,24 @@
 package gsg.corp.driver_presentation.screens.routes.routes_detail
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -217,6 +227,16 @@ fun ButtonRecolectado() {
 
 @Composable
 fun PhotoCollect() {
+    var selectedImage by remember { mutableStateOf(true) }
+    val context = LocalContext.current
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    var bitmap by remember{ mutableStateOf<Bitmap?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()){
+        imageUri = it
+    }
+
     Column(modifier = Modifier
         .fillMaxWidth()
         .padding(start = 47.dp, top = 16.dp)
@@ -228,9 +248,25 @@ fun PhotoCollect() {
             modifier = Modifier
                 .height(10.dp)
         )
-        Row() {
-            Image(painter =painterResource(id = R.drawable.image) , contentDescription = null
-            )
+        Row {
+             Box(modifier = Modifier.size(120.dp)){
+                 if(selectedImage) {
+                     Image(painter =painterResource(id = R.drawable.image) , contentDescription = null)
+                 }
+                 imageUri?.let {
+                     bitmap = if(Build.VERSION.SDK_INT < 28){
+                         MediaStore.Images.Media.getBitmap(context.contentResolver,it)
+                     }else {
+                         selectedImage = false
+                         val source = ImageDecoder.createSource(context.contentResolver,it)
+                         ImageDecoder.decodeBitmap(source)
+
+                     }
+                     if(!selectedImage) {
+                         Image(bitmap = bitmap?.asImageBitmap()!!, contentDescription = "" , modifier = Modifier.size(200.dp))
+                     }
+                 }
+             }
 
             Column(
                 Modifier,
@@ -262,6 +298,7 @@ fun PhotoCollect() {
                 Row(
                     Modifier
                         .padding(start = 25.dp, end = 24.dp)
+                        .clickable { launcher.launch("image/*") }
                 ){
                     Icon(
                         painter = painterResource(id = R.drawable.galleryexport),
